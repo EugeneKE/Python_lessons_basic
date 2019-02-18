36bfa35de7658e6d91f472333f4ab1d44961e2d0
EugeneKE <47592344+EugeneKE@users.noreply.github.com>
2019-02-18 20:07:24 +0300

    Update hw02_hard.py

--- lesson02/home_work/hw02_hard.py
+++ lesson02/home_work/hw02_hard.py
@@ -1,9 +1,11 @@
+_autor_='Кувалдин Е.А.'
 # Задание-1: уравнение прямой вида y = kx + b задано в виде строки.
 # Определить координату y точки с заданной координатой x.
-
+print("задача №1")
 equation = 'y = -12x + 11111140.2121'
 x = 2.5
 # вычислите и выведите y
+print('y = {}'.format(x * (-12) + 11111140.2121))
 
 
 # Задание-2: Дата задана в виде строки формата 'dd.mm.yyyy'.
@@ -17,12 +19,49 @@ x = 2.5
 #  (т.е. 2 символа для дня, 2 - для месяца, 4 - для года)
 
 # Пример корректной даты
-date = '01.11.1985'
+#date = '01.11.1985'
 
-# Примеры некорректных дат
-date = '01.22.1001'
-date = '1.12.1001'
-date = '-2.10.3001'
+# ...Примеры некорректных дат
+#date = '01.22.1001'
+#date = '1.12.1001'
+#date = '-2.10.3001'
+print('Задача №2')
+def yearFiller (start, end):
+    ylf = []
+    b = ''
+    for i in range(start, end):
+        if len(str(i)) < 4:
+            b = (4 - len(str(i)))*'0' + str(i)
+            ylf.append(b)
+        else:
+            ylf.append((str(i)))
+    return ylf
+def myFillOuter (start, end):
+    mlf = []
+    b = ''
+    for i in range(start, end + 1):
+        if len(str(i)) == 1:
+            b = '0' + str(i)
+            mlf.append(b)
+        else:
+            mlf.append(str(i))
+    return mlf
+
+myDataRange = myFillOuter(1, 31)
+myMonthRange = myFillOuter(1, 12)
+myYarRange = yearFiller(1, 9999)
+
+def myChekker(strdata):
+    if (strdata[:2] in myDataRange) == True:
+        if (strdata[3:5] in myMonthRange) == True:
+            if(strdata[6:] in myYarRange) == True:
+                print('Дата {} - вправильном формате'.format(strdata))
+##Парианты проверки: выводиться будут даты тоолько в правильном формате
+Checkdate = '01.11.1985'
+myChekker(Checkdate)
+myChekker('23.07.1974')
+
+
 
 
 # Задание-3: "Перевёрнутая башня" (Задача олимпиадного уровня)
@@ -54,3 +93,18 @@ date = '-2.10.3001'
 #
 # Вход: 11
 # Выход: 5 3
+
+#myTower = {}
+#r = 1 # номер комнаты
+#myFlore = 1 # номер этажа
+#rooms = [] # номера комнат на этаже
+#for i in range(1, 100):
+#    while myFlore < (myFlore * i):
+ #       rooms.append(r)
+ #       r += 1
+ #   myTower.update({myFlore: rooms})
+ #   myFlore +=1
+
+
+
+
